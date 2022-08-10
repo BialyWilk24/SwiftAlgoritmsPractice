490da4196d95730400fff7cfd2f24dd9f744a708
Maksim <wolfqest24@gmail.com>
2022-08-10 22:06:57 +0300

    Simple binary search on Swift

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,14 +1,15 @@
 import UIKit
+import Foundation
 
-var SortedArray: [Int] = []
+var Array: [Int] = []
 
 for i in 0...150 {
-    SortedArray.append(i)
+    Array.append(i)
 }
 
-var Array2 = [1, 5, 7, 3, 9, 24]
+var Array2 = [1, 5, 7, 3, 9, 24, 42, 73, 123]
 
-SortedArray.count
+Array.count
 
 func BinarySearch(Array: [Int], Value: Int) -> Int? {
     var low = 0
@@ -16,6 +17,7 @@ func BinarySearch(Array: [Int], Value: Int) -> Int? {
     while low <= high {
         let mid = (low + high)
         let guess = Array[mid]
+        
         if guess == Value {
             return mid
         }
@@ -28,7 +30,6 @@ func BinarySearch(Array: [Int], Value: Int) -> Int? {
     
     return nil
 }
-
 BinarySearch(Array: Array2, Value: 24)
 
 
